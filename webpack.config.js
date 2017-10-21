284557a8b402a83e227a0dfd3fd49c206d83c40e
James <jwinham4740@gmail.com>
2017-10-20 23:30:30 -0400

    bare bones

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import our plugin -> ADDED IN THIS STEP
+
 const paths = {
     DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
     JS: path.resolve(__dirname, 'src/js')
 }
 
@@ -11,6 +14,35 @@ module.exports = {
     output: {
         path: paths.DIST,
         filename: "app.bundle.js"
-    }
+    },
+    // devServer: {
+    //     contentBase: paths.SRC,
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
+    // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
+    //
+    // So we can write:
+    // import MyComponent from './my-component';
+    //
+    // Instead of:
+    // import MyComponent from './my-component.jsx';
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 };
 
